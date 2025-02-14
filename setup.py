e193d74c5536d97a23357295b77d685bc6eaa3c5
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2025-02-14 16:19:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-# my_ml_package/setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name='DipoleAmplitudePredictor',
-    version='0.1',
+    version='0.2',  # Increment version to reflect changes
     packages=find_packages(),
     install_requires=[
         'numpy',
         'scikit-learn',
+        'boto3',  # Added new dependencies
     ],
     description='A package for making predictions for Dipole Amplitude using a pre-trained Random Forest model',
     author='Aryan Patil',
-    author_email='aryansanjay.patil@stonybrook.edu', 
+    author_email='aryansanjay.patil@stonybrook.edu',
 )
